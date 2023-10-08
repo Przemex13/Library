6b87d48be385540e8b41e115c0335a1f2b3afb14
Przemyslaw Poskrobko <przemyslaw.poskrobko@gmail.com>
2023-10-08 16:03:10 +0200

    New Copy, wczytuje elegancko

--- src/NewCopy.java
+++ src/NewCopy.java
@@ -1,3 +1,5 @@
+import LibraryFiles.DatabaseConnector;
+
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -10,7 +12,7 @@ public class NewCopy {
     private JTable bookTable;
     private JButton addBookButton;
     private JTextField idBookTextField;
-    static int selectRow = Integer.parseInt(null);
+    static int selectRow;
 
 
     public NewCopy() {
@@ -32,7 +34,69 @@ public class NewCopy {
 
     }
 
-    public static void main(String[] args) {
-        new NewCopy();
+    public class TitlesAndAuthors {
+
+            private JPanel panel1;
+            JTable authorsAndTitels;
+            private JButton addNewButton;
+            private JButton chooseButton;
+            int selectedRow;
+
+            public static boolean ifAuthorTitleDataIsCorrect(String author, String title) {
+                boolean isCorrect = true;
+                DatabaseConnector databaseConnector = new DatabaseConnector();
+                return isCorrect;
+            }
+
+            public JTable getAuthorsAndTitels() {
+                return authorsAndTitels;
+            }
+
+            public TitlesAndAuthors() {
+                JFrame frame = new JFrame("Titles and Authors");
+                frame.setContentPane(panel1);
+                frame.setVisible(true);
+                frame.setSize(450, 400);
+                DatabaseConnector databaseConnector = new DatabaseConnector("select * from booktable;", authorsAndTitels);
+                addNewButton.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        CustomDialog customDialog = new CustomDialog();
+
+                        int result = JOptionPane.showConfirmDialog(null, customDialog.contentPane, "", JOptionPane.OK_CANCEL_OPTION);
+                        System.out.println(result);
+                        if (result == JOptionPane.OK_OPTION) {
+                            String author = customDialog.getAuthor();
+                            String title = customDialog.getTitleText();
+                            String sqlQuery = String.format("insert into booktable(`author`, `title`) VALUES ('%s', '%s')", author, title);
+                            System.out.println(author);
+                            System.out.println(title);
+                            DatabaseConnector databaseConnector1 = new DatabaseConnector(sqlQuery);
+                            DatabaseConnector databaseConnector = new DatabaseConnector("select * from booktable;", authorsAndTitels);
+                        }
+                    }
+                });
+                authorsAndTitels.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        selectedRow = authorsAndTitels.getSelectedRow();
+                        System.out.println(selectedRow);
+                    }
+                });
+                chooseButton.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        idBookTextField.setText(String.valueOf(authorsAndTitels.getValueAt(selectedRow, 0)));
+                        authorTextField.setText(String.valueOf(authorsAndTitels.getValueAt(selectedRow, 1)));
+                        titleTextField.setText(String.valueOf(authorsAndTitels.getValueAt(selectedRow, 2)));
+                        frame.dispose();
+//                System.out.println(titlesAndAuthors.authorsAndTitels.getValueAt(titlesAndAuthors.authorsAndTitels.getSelectedRow(),1));
+                    }
+                });
+            }
+        }
+
+        public static void main(String[] args) {
+            new NewCopy();
+        }
     }
-}
